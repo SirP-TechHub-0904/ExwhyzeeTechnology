911c5607f8334ffb330ed4e77e0897a7b5d63f28
VS-GitHub-001 <105536514+VS-GitHub-001@users.noreply.github.com>
2024-07-24 13:02:03 +0100

    Add project files.

---
+++ src/ExwhyzeeTechnology.Website/Pages/OpeTest.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ExwhyzeeTechnology.Domain.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ExwhyzeeTechnology.Website.Pages
+{
+    [AllowAnonymous]
+    public class OpeTestModel : PageModel
+    {
+        private readonly UserManager<Profile> _userManager;
+
+        private readonly IEmailSender _sender;
+        public OpeTestModel(IEmailSender sender, UserManager<Profile> userManager)
+        {
+            _sender = sender;
+            _userManager = userManager;
+        }
+        public bool Result = false;
+        public async Task<IActionResult> OnGetAsync()
+        {
+           
+             await _sender.SendEmailAsync("onwukaemeka41@gmail.com", "Test Mail Message", "Email Test");
+            return Page();
+        }
+    }
+}
